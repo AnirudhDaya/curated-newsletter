bdfa53cb96fb2e91b9ee79284cc35bd703e421fa
Anirudh Dayanand <anirudhdaya@gmail.com>
2023-11-05 15:51:19 +0530

    Added menu switching

--- javascript/script.js
+++ javascript/script.js
@@ -15,6 +15,30 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
 
+    document.querySelectorAll('.popup-content-item').forEach(function (categoryLink) {
+      categoryLink.addEventListener('click', function () {
+        // Get the category name from the clicked link
+        popup.style.display = 'none';
+        const category = categoryLink.textContent.toLowerCase();
+  
+        // Hide all cards
+        document.querySelectorAll('.card').forEach(function (card) {
+          const cardCategory = card.querySelector('.card__category').textContent.toLowerCase();
+        
+          if (cardCategory === category) {
+            card.style.display = 'block';
+          } else {
+            card.style.display = 'none';
+          }
+        });
+  
+        // Show cards of the selected category
+        document.querySelectorAll('.' + category).forEach(function (card) {
+          card.style.display = 'block';
+        });
+      });
+    });
+
     closePopup.addEventListener('click', function () {
         popup.style.display = 'none';
       });
